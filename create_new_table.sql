9ee5d9a79ef2d6a10688964d758875003d618d4d
priyathamnamakkal <134202979+priyathamnamakkal@users.noreply.github.com>
2026-05-12 20:00:28 +0530

    Update create_new_table.sql

--- create_new_table.sql
+++ create_new_table.sql
@@ -1,19 +1,19 @@
 USE [MyDatabase]
 GO
 
-/****** Object:  Table [dbo].[persions]    Script Date: 12-05-2026 15:57:27 ******/
+/****** Object:  Table [dbo].[persons]    Script Date: 12-05-2026 15:57:27 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[persions](
+CREATE TABLE [dbo].[persons](
 	[id] [int] NOT NULL,
-	[persion_name] [varchar](50) NOT NULL,
+	[persons_name] [varchar](50) NOT NULL,
 	[birth_date] [date] NULL,
 	[phone_number] [varchar](15) NOT NULL,
- CONSTRAINT [pk_persions] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [pk_persons] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
